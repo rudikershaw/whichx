fb947a06e19d7923915189c35a5f54f10a6a3747
Rudi Kershaw <rudi.kershaw@cognitran.com>
2018-06-22 22:42:53 +0100

    Rudi Kershaw - Add Whichpet constructor related unit tests.

--- test/tests.js
+++ test/tests.js
@@ -3,10 +3,24 @@
     var Whichpet = require("../assets/scripts/whichpet.js");
 
     describe("Whichpet", function() {
-        describe("constructor", function () {
-            it("should create an object", function () {
-                var classifier = new Whichpet();
+        describe("constructor", function() {
+            var classifier = new Whichpet();
+            it("should create an object", function() {
                 assert.equal(typeof classifier, "object");
+            });
+
+            it("should create a unique object", function() {
+                var newClassifier = new Whichpet();
+                newClassifier.property = 1;
+                assert.equal(classifier.property, undefined);
+            });
+
+            it("object should have 3 main methods", function() {
+                assert.ok(classifier.addData && classifier.addLabels && classifier.classify);
+            });
+
+            it("object should have key vars hidden", function() {
+                assert.ok(!(classifier.typesMap) && !(classifier.STOPWORDS) && !(classifier.processToArray));
             });
         });
     });
